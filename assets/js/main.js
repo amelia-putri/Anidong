1023b91027d809590f7922dc8c47ecdad85183bc
amelia-putri <ameliaputrinajwa21@gmail.com>
2026-01-01 16:34:30 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
-fetch('data/anime.json')
-  .then(res => res.json())
-  .then(data => {
-    let html = '';
-    data.forEach(a => {
-      html += `
-        <a href="anime.html?id=${a.id}">
-          <div class="hover:scale-105 transition">
-            <img src="${a.cover}" class="rounded">
-            <h3 class="mt-2 text-center">${a.title}</h3>
-          </div>
-        </a>
-      `;
-    });
-    document.getElementById('list').innerHTML = html;
-  })
-  .catch(err => console.error(err));
+html += `
+<a href="anime.html?id=${a.id}" class="group">
+  <div class="bg-zinc-900 rounded-lg overflow-hidden shadow hover:shadow-xl transition">
+    <div class="relative">
+      <img src="${a.cover}" class="w-full h-48 object-cover group-hover:scale-105 transition duration-300">
+      <span class="absolute top-2 left-2 bg-red-600 text-xs px-2 py-1 rounded">
+        ONGOING
+      </span>
+    </div>
+    <div class="p-2">
+      <h3 class="text-sm font-semibold line-clamp-2 group-hover:text-red-500">
+        ${a.title}
+      </h3>
+    </div>
+  </div>
+</a>
+`;

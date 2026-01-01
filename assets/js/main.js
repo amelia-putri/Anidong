21a76c01879796f7b1c98212b96e6bf3fe4b8ee0
amelia-putri <ameliaputrinajwa21@gmail.com>
2026-01-01 19:58:05 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,49 @@
+let animeData = [];
+
+const list = document.getElementById("anime-list");
+const search = document.getElementById("search");
+
 fetch("./data/anime.json")
   .then(res => res.json())
   .then(data => {
-    const list = document.getElementById("anime-list");
-    list.innerHTML = "";
-
-    data.forEach(anime => {
-      const lastEp = anime.episodes[0];
-
-      list.innerHTML += `
-        <a href="detail.html?id=${anime.id}" class="block bg-neutral-800 rounded overflow-hidden hover:scale-105 transition">
-          <img src="${anime.cover}" class="w-full h-40 object-cover">
-          <div class="p-3">
-            <h3 class="font-bold">${anime.title}</h3>
-            <p class="text-sm text-gray-400">Episode ${lastEp.episode}</p>
-          </div>
-        </a>
-      `;
-    });
+    animeData = data;
+    renderAnime(animeData);
   })
   .catch(() => {
-    document.getElementById("anime-list").innerHTML =
-      "<p class='text-red-500'>Gagal load data</p>";
+    list.innerHTML = "<p class='text-red-500'>Gagal load data</p>";
   });
+
+function renderAnime(data) {
+  list.innerHTML = "";
+
+  if (data.length === 0) {
+    list.innerHTML = "<p class='text-gray-400'>Anime tidak ditemukan</p>";
+    return;
+  }
+
+  data.forEach(anime => {
+    const lastEp = anime.episodes[0];
+
+    list.innerHTML += `
+      <a href="detail.html?id=${anime.id}"
+        class="block bg-neutral-800 rounded overflow-hidden hover:scale-105 transition">
+        <img src="${anime.cover}" class="w-full h-40 object-cover">
+        <div class="p-3">
+          <h3 class="font-bold">${anime.title}</h3>
+          <p class="text-sm text-gray-400">Episode ${lastEp.episode}</p>
+        </div>
+      </a>
+    `;
+  });
+}
+
+// SEARCH LOGIC
+search.addEventListener("input", e => {
+  const keyword = e.target.value.toLowerCase();
+
+  const filtered = animeData.filter(a =>
+    a.title.toLowerCase().includes(keyword)
+  );
+
+  renderAnime(filtered);
+});

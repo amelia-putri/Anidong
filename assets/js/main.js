6e0e599ef3e4fdc39cf6677c77f9d6224e87268c
amelia-putri <ameliaputrinajwa21@gmail.com>
2026-01-01 17:20:18 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,10 +6,11 @@ fetch("./data/anime.json")
   .then(data => {
     data.forEach(item => {
       html += `
-        <a href="anime.html?id=${item.id}" class="group">
+        <a href="data/anime.html?id=${item.id}" class="group">
           <div class="bg-zinc-900 rounded-lg overflow-hidden shadow hover:shadow-xl transition">
             <div class="relative">
-              <img src="${item.cover}" class="w-full h-48 object-cover group-hover:scale-105 transition duration-300">
+              <img src="${item.cover}"
+                   class="w-full h-48 object-cover group-hover:scale-105 transition duration-300">
               <span class="absolute top-2 left-2 bg-red-600 text-xs px-2 py-1 rounded">
                 ONGOING
               </span>
@@ -24,6 +25,7 @@ fetch("./data/anime.json")
       `;
     });
 
+    // ⬅️ WAJIB DI LUAR forEach
     container.innerHTML = html;
   })
   .catch(err => console.error(err));

9e3e5d5461ec93eb56722363090e5ec30756f09f
amelia-putri <ameliaputrinajwa21@gmail.com>
2026-01-01 16:15:40 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,17 @@
 fetch('data/anime.json')
-.then(res => res.json())
-.then(data => {
-  let html = '';
-  data.forEach(a => {
-    html += `
-      <a href="anime.html?id=${a.id}">
-        <div class="hover:scale-105 transition">
-          <img src="${a.cover}" class="rounded">
-          <h3 class="mt-2 text-center">${a.title}</h3>
-        </div>
-      </a>
-    `;
-  });
-  document.getElementById('list').innerHTML = html;
-});
+  .then(res => res.json())
+  .then(data => {
+    let html = '';
+    data.forEach(a => {
+      html += `
+        <a href="anime.html?id=${a.id}">
+          <div class="hover:scale-105 transition">
+            <img src="${a.cover}" class="rounded">
+            <h3 class="mt-2 text-center">${a.title}</h3>
+          </div>
+        </a>
+      `;
+    });
+    document.getElementById('list').innerHTML = html;
+  })
+  .catch(err => console.error(err));

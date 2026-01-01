1be541ecce23efc243e00155242799f44a776ca0
amelia-putri <ameliaputrinajwa21@gmail.com>
2026-01-01 16:16:41 +0700

    Update watch.js

--- assets/js/watch.js
+++ assets/js/watch.js
@@ -3,9 +3,9 @@ const id = params.get('id');
 const ep = params.get('ep');
 
 fetch('data/anime.json')
-.then(res => res.json())
-.then(data => {
-  const anime = data.find(a => a.id === id);
-  const episode = anime.episodes.find(e => e.ep == ep);
-  document.getElementById('player').src = episode.url;
-});
+  .then(res => res.json())
+  .then(data => {
+    const anime = data.find(a => a.id === id);
+    const episode = anime.episodes.find(e => e.ep == ep);
+    document.getElementById('player').src = episode.url;
+  });

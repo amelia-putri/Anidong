1d9a46433dd39c99328332dc3be95ffd528618c3
amelia-putri <ameliaputrinajwa21@gmail.com>
2026-01-01 16:16:20 +0700

    Update anime.js

--- assets/js/anime.js
+++ assets/js/anime.js
@@ -1,19 +1,19 @@
 const id = new URLSearchParams(location.search).get('id');
 
 fetch('data/anime.json')
-.then(res => res.json())
-.then(data => {
-  const anime = data.find(a => a.id === id);
-  document.getElementById('title').textContent = anime.title;
-  document.getElementById('cover').src = anime.cover;
+  .then(res => res.json())
+  .then(data => {
+    const anime = data.find(a => a.id === id);
+    document.getElementById('title').textContent = anime.title;
+    document.getElementById('cover').src = anime.cover;
 
-  let eps = '';
-  anime.episodes.forEach(e => {
-    eps += `
-      <a href="watch.html?id=${id}&ep=${e.ep}" class="bg-zinc-800 p-2 text-center rounded">
-        EP ${e.ep}
-      </a>
-    `;
+    let eps = '';
+    anime.episodes.forEach(e => {
+      eps += `
+        <a href="watch.html?id=${id}&ep=${e.ep}" class="bg-zinc-800 p-2 text-center rounded">
+          EP ${e.ep}
+        </a>
+      `;
+    });
+    document.getElementById('episodes').innerHTML = eps;
   });
-  document.getElementById('episodes').innerHTML = eps;
-});

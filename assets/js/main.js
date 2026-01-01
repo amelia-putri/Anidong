2dd54fdb20e8b30e2f8bd9b609afb3148a38b82c
amelia-putri <ameliaputrinajwa21@gmail.com>
2026-01-01 19:49:33 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const list = document.getElementById("list");
-
-  fetch("./data/anime.json")
+fetch("./data/anime.json")
   .then(res => res.json())
   .then(data => {
-    // render anime
+    const list = document.getElementById("anime-list");
+    list.innerHTML = "";
+
+    data.forEach(anime => {
+      const lastEp = anime.episodes[0];
+
+      list.innerHTML += `
+        <a href="detail.html?id=${anime.id}" class="block bg-neutral-800 rounded overflow-hidden hover:scale-105 transition">
+          <img src="${anime.cover}" class="w-full h-40 object-cover">
+          <div class="p-3">
+            <h3 class="font-bold">${anime.title}</h3>
+            <p class="text-sm text-gray-400">Episode ${lastEp.episode}</p>
+          </div>
+        </a>
+      `;
+    });
   })
-  .catch(err => {
-    console.error(err);
+  .catch(() => {
     document.getElementById("anime-list").innerHTML =
       "<p class='text-red-500'>Gagal load data</p>";
   });
-
-
-      data.forEach(item => {
-        html += `
-          <a href="detail.html?id=${item.id}"
-             class="block bg-neutral-800 rounded-xl overflow-hidden shadow-lg hover:scale-105 transition">
-            
-            <img
-              src="${item.cover}"
-              alt="${item.title}"
-              class="w-full h-48 object-cover"
-            />
-
-            <div class="p-4">
-              <h3 class="font-bold text-sm text-white">${item.title}</h3>
-              <p class="text-xs text-gray-400 mt-1">
-                ${item.episodes.length} Episode
-              </p>
-            </div>
-          </a>
-        `;
-      });
-
-      list.innerHTML = html;
-    })
-    .catch(err => {
-      console.error(err);
-      list.innerHTML = "<p class='text-red-500'>Gagal load data</p>";
-    });
-});

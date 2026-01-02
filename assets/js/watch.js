efbc7eff22ae2c3fdd88f9d344cdb64dd7cbcf6b
amelia-putri <ameliaputrinajwa21@gmail.com>
2026-01-02 11:13:27 +0700

    Update watch.js

--- assets/js/watch.js
+++ assets/js/watch.js
@@ -2,7 +2,7 @@ const params = new URLSearchParams(location.search);
 const id = params.get('id');
 const ep = params.get('ep');
 
-fetch('data/anime.json')
+fetch("data/anime.json")
   .then(res => res.json())
   .then(data => {
     const anime = data.find(a => a.id === id);

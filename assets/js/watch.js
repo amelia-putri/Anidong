73c550df0dee9df1cdd14e1a403ab372949b5475
amelia-putri <ameliaputrinajwa21@gmail.com>
2026-01-02 14:41:54 +0700

    Update watch.js

--- assets/js/watch.js
+++ assets/js/watch.js
@@ -1,11 +1,30 @@
-const params = new URLSearchParams(location.search);
-const id = params.get('id');
-const ep = params.get('ep');
+const p = new URLSearchParams(location.search);
+const id = p.get("id");
+const ep = parseInt(p.get("ep"));
 
 fetch("data/anime.json")
   .then(res => res.json())
   .then(data => {
+
     const anime = data.find(a => a.id === id);
-    const episode = anime.episodes.find(e => e.ep == ep);
-    document.getElementById('player').src = episode.url;
+    const current = anime.episodes.find(e => e.ep === ep);
+
+    epTitle.innerText = `${anime.title} Episode ${ep}`;
+    video.innerHTML = current.video;
+
+    anime.episodes.forEach(e=>{
+      listEp.innerHTML += `
+        <div class="episode"
+          onclick="location.href='watch.html?id=${id}&ep=${e.ep}'">
+          Episode ${e.ep}
+        </div>
+      `;
+    });
+
+    const next = anime.episodes.find(e=>e.ep===ep+1);
+    if(next){
+      nextEp.href = `watch.html?id=${id}&ep=${ep+1}`;
+    } else {
+      nextEp.style.display="none";
+    }
   });

e303120458ad9af5891e5245e19191e7111f6b28
amelia-putri <ameliaputrinajwa21@gmail.com>
2026-01-02 14:41:03 +0700

    Update detail.js

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -3,28 +3,25 @@ const id = new URLSearchParams(location.search).get("id");
 fetch("data/anime.json")
   .then(res => res.json())
   .then(data => {
-    const anime = data.find(a => a.id === id);
 
-    document.getElementById("cover").src = anime.cover;
-    document.getElementById("title").innerText = anime.title;
-    document.getElementById("desc").innerText = anime.desc;
+    const anime = data.find(x => x.id === id);
 
-    const epList = document.getElementById("episodeList");
+    cover.src = anime.cover;
+    title.innerText = anime.title;
+    desc.innerText = anime.desc;
 
     anime.episodes
-      .sort((a, b) => b.ep - a.ep) // EP BARU KE ATAS
-      .forEach(ep => {
-        epList.innerHTML += `
+      .sort((a,b)=>b.ep-a.ep)
+      .forEach(e => {
+        episodeList.innerHTML += `
           <div class="episode"
-            onclick="location.href='watch.html?id=${anime.id}&ep=${ep.ep}'">
-            Episode ${ep.ep}
+            onclick="location.href='watch.html?id=${id}&ep=${e.ep}'">
+            Episode ${e.ep}
           </div>
         `;
       });
 
-    // SIDEBAR
-    const sidebar = document.getElementById("sidebarList");
-    data.slice(0, 6).forEach(a => {
-      sidebar.innerHTML += `<p>${a.title}</p>`;
+    data.slice(0,6).forEach(a=>{
+      sideList.innerHTML += `<p>${a.title}</p>`;
     });
   });
